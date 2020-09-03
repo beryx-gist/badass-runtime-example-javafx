78f6485ccc99d486c58498af485a3792bdf88e10
Serban Iordache <serban.iordache@gmail.com>
2020-09-03 16:23:15 +0200

    configure secondary launcher

---
+++ src/main/java/org/example/hellofx/HelloFX2.java
@@ -0,0 +1,29 @@
+package org.example.hellofx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloFX2 extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hellofx2.fxml"));
+        stage.setTitle("Hello World - Secondary");
+        stage.getIcons().add(new Image("hellofx2.png"));
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/org/example/hellofx/Launcher2.java
@@ -0,0 +1,7 @@
+package org.example.hellofx;
+
+public class Launcher2 {
+    public static void main(String[] args) {
+        org.example.hellofx.HelloFX2.main(args);
+    }
+}
